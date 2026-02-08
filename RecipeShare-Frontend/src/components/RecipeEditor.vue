d0ac5c0bbf19df4eeb7348aaeab3f953f926961f
raklan <rcl.catcher@gmail.com>
2026-02-08 12:37:31 -0700

    Disable image saving

--- RecipeShare-Frontend/src/components/RecipeEditor.vue
+++ RecipeShare-Frontend/src/components/RecipeEditor.vue
@@ -264,23 +264,25 @@ onMounted(() => {
                     <input id="private-checkbox" type="checkbox" class="form-check mx-2" :checked="vm.recipe.private"
                         v-model="vm.recipe.private" />
                 </div>
-                <img v-if="vm.recipe?.image" :src="`${props.apiUrl}/recipepicture/${vm.recipe?.image}`" style="max-width: 50%; max-height: 50%"></img>
-                <p v-if="vm.recipe?.image" style="cursor:pointer; text-decoration: underline; color:blue" @click="deleteImage">Remove Image</p>
-                <form v-if="!vm.recipe?.image" class="file-upload-form">
-                    <label for="picture-upload" class="file-upload-label">
-                        <div class="file-upload-design">
-                            <svg viewBox="0 0 640 512" height="1em">
-                                <path
-                                    d="M144 480C64.5 480 0 415.5 0 336c0-62.8 40.2-116.2 96.2-135.9c-.1-2.7-.2-5.4-.2-8.1c0-88.4 71.6-160 160-160c59.3 0 111 32.2 138.7 80.2C409.9 102 428.3 96 448 96c53 0 96 43 96 96c0 12.2-2.3 23.8-6.4 34.6C596 238.4 640 290.1 640 352c0 70.7-57.3 128-128 128H144zm79-217c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l39-39V392c0 13.3 10.7 24 24 24s24-10.7 24-24V257.9l39 39c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-80-80c-9.4-9.4-24.6-9.4-33.9 0l-80 80z">
-                                </path>
-                            </svg>
-                            <p>Drag and Drop</p>
-                            <p>or</p>
-                            <span class="browse-button">Pick an Image</span>
-                        </div>
-                        <input id="picture-upload" type="file" v-on:change="imageChange" accept=".png,.jpg,.jpeg"/>
-                    </label>
-                </form>
+                <div v-if="false"> <!-- Temporarily disabled until images are fixed -->
+                    <img v-if="vm.recipe?.image" :src="`${props.apiUrl}/recipepicture/${vm.recipe?.image}`" style="max-width: 50%; max-height: 50%"></img>
+                    <p v-if="vm.recipe?.image" style="cursor:pointer; text-decoration: underline; color:blue" @click="deleteImage">Remove Image</p>
+                    <form v-if="!vm.recipe?.image" class="file-upload-form">
+                        <label for="picture-upload" class="file-upload-label">
+                            <div class="file-upload-design">
+                                <svg viewBox="0 0 640 512" height="1em">
+                                    <path
+                                        d="M144 480C64.5 480 0 415.5 0 336c0-62.8 40.2-116.2 96.2-135.9c-.1-2.7-.2-5.4-.2-8.1c0-88.4 71.6-160 160-160c59.3 0 111 32.2 138.7 80.2C409.9 102 428.3 96 448 96c53 0 96 43 96 96c0 12.2-2.3 23.8-6.4 34.6C596 238.4 640 290.1 640 352c0 70.7-57.3 128-128 128H144zm79-217c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l39-39V392c0 13.3 10.7 24 24 24s24-10.7 24-24V257.9l39 39c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-80-80c-9.4-9.4-24.6-9.4-33.9 0l-80 80z">
+                                    </path>
+                                </svg>
+                                <p>Drag and Drop</p>
+                                <p>or</p>
+                                <span class="browse-button">Pick an Image</span>
+                            </div>
+                            <input id="picture-upload" type="file" v-on:change="imageChange" accept=".png,.jpg,.jpeg"/>
+                        </label>
+                    </form>
+                </div>
 
             </div>
 
