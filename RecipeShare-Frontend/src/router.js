c067f1d43b01270e2df827d2fa11bd606dbe1c5d
raklan <rcl.catcher@gmail.com>
2026-02-08 12:17:55 -0700

    Add _routes.json

--- RecipeShare-Frontend/src/router.js
+++ RecipeShare-Frontend/src/router.js
@@ -1,4 +1,4 @@
-import { createWebHashHistory, createRouter } from 'vue-router'
+import { createWebHistory, createRouter } from 'vue-router'
 
 import Home from './components/Home.vue'
 import NotFound from './components/NotFound.vue'
@@ -55,7 +55,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHashHistory(),    
+    history: createWebHistory(),    
     routes,
 })
 
