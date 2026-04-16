7fc4cfda8600b421c004683f7482bfee2e62edec
raklan <rcl.catcher@gmail.com>
2026-04-15 19:21:23 -0600

    Update cors for backend

--- RecipeShare-Backend/app.js
+++ RecipeShare-Backend/app.js
@@ -20,7 +20,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-    origin: ['http://192.168.0.77:5173', 'http://localhost:5173', 'https://recipeshare-frontend.rcl-catcher.workers.dev'],
+    origin: ['http://192.168.0.77:5173', 'http://localhost:5173', 'https://recipeshare-frontend.rcl-catcher.workers.dev', 'bearshark.xyz'],
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }
 
